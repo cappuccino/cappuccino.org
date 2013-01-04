665a0d2f13a5322d06b1d9ea0c85c7d434a003b0
Alexander Ljungberg <aljungberg@slevenbits.com>
2013-01-04 01:33:07 +0000

    Minify CSS and JS and hash tag all static resources for caching.

--- config.rb
+++ config.rb
@@ -92,10 +92,13 @@ end
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
+
+  activate :asset_hash,
+    :ignore => [/blog\/uploads\/.*/, /downloads\/.*/]
 
   # Enable cache buster
   # activate :cache_buster
